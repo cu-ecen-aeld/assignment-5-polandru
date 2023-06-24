c2406a84c9ad6726eb71cacaa46bd58e45305b26
polandru <paulperdomo@comcast.net>
2023-06-24 10:40:41 -0500

    Trying to fix buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 27869eab48c1d19cba49d113037e541b7d2dd080 
+AESD_ASSIGNMENTS_VERSION = 97aa3ca8f9dca20e3f976a3e3d68fa31d268c559
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -15,24 +15,19 @@ AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=aarch64-none-linux-gnu- -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILE=aarch64-none-linux-gnu- -C $(@D)/server all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-	$(INSTALL) -m 0755 $(@D)/finder-app/writer ${TARGET_DIR}/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh ${TARGET_DIR}/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh ${TARGET_DIR}/usr/bin/
-	@mkdir -p ${TARGET_DIR}/root/conf 
-	$(INSTALL) -m 0755 $(@D)/finder-app/conf/username.txt ${TARGET_DIR}/root/conf/username.txt
+	
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/aesdsocket
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/finder-app/reqlibs/ld-linux-aarch64.so.1 ${TARGET_DIR}/lib/
 	$(INSTALL) -m 0755 $(@D)/finder-app/reqlibs/libc.so.6 ${TARGET_DIR}/lib64/
 	$(INSTALL) -m 0755 $(@D)/finder-app/reqlibs/libm.so.6 ${TARGET_DIR}/lib64/
 	$(INSTALL) -m 0755 $(@D)/finder-app/reqlibs/libresolv.so.2 ${TARGET_DIR}/lib64/
-	
+	@echo hello
 endef
 
 $(eval $(generic-package))
