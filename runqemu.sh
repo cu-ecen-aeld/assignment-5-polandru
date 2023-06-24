9354c61e4e7c289d99a9ae5f112e97543a3bb902
polandru <paulperdomo@comcast.net>
2023-06-24 08:49:12 -0500

    Assignment 5 Part 2 Finished

--- runqemu.sh
+++ runqemu.sh
@@ -8,8 +8,9 @@ qemu-system-aarch64 \
     -M virt  \
     -cpu cortex-a53 -nographic -smp 1 \
     -kernel buildroot/output/images/Image \
+    -m 512M \
     -append "rootwait root=/dev/vda console=ttyAMA0" \
-    -netdev user,id=eth0,hostfwd=tcp::10022-:22 \
+    -netdev user,id=eth0,hostfwd=tcp::10022-:22,hostfwd=tcp::9000-:9000 \
     -device virtio-net-device,netdev=eth0 \
     -drive file=buildroot/output/images/rootfs.ext4,if=none,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 -device virtio-rng-pci
